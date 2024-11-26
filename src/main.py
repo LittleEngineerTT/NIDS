366ed46b747b661256028b5e3138161b8aee1246
wesleyg97322 <wesley@lac-coloc.fr>
2024-11-26 14:54:43 +0100

    feat: add key arg to encrypt the mail

--- src/main.py
+++ src/main.py
@@ -7,8 +7,11 @@ def run():
         description="This script executes a defensive script is executed to intercept scan SYN, DDOS and brute force attack on the honeypot.")
     parser.add_argument('--mail', "-mail",nargs='?', type=str,
                         help='On which email to send the report', required=True)
+    parser.add_argument('--key', "-key",nargs='?', type=str,
+                        help='Key to encrypt the mail', required=True)
     args = parser.parse_args()
-    print(args.mail)
+    print(f"Mail: {args.mail}")
+    print(f"Key: {args.key}")
 
 
 if __name__ == "__main__":
