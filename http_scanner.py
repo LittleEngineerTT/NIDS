3e1deebcb5d74516b7d0f473d9feded92397b827
wesleyg97322 <wesley@lac-coloc.fr>
2024-11-29 14:17:39 +0100

    feat: simple sy scan

---
+++ http_scanner.py
@@ -0,0 +1,45 @@
+import socket
+import time
+import sys
+
+
+def scan_port(ip: str, port: int, delay:int = 0):
+    try:
+        # socket creation
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+
+        # Connection
+        result = sock.connect_ex((ip, port))
+
+        if result == 0:
+            print(f"The {port} is open")
+        else:
+            print(f"The {port} is closed")
+
+    except socket.error:
+        print(f"Socket connection error")
+    finally:
+        sock.close()
+
+    time.sleep(delay)
+
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3 and len(sys.argv) != 4:
+        print("Usage: python script.py <ip> <ports> <scan_type>")
+        print("scan_type:\n" +
+                "simple\n")
+        sys.exit(1)
+
+    target_ip = sys.argv[1]
+    tmp_target_port = sys.argv[2]
+    target_port = []
+    scan_type = sys.argv[3] if len(sys.argv) == 4 else "simple"
+
+    print(f"Syn scan to {target_ip} on {target_port} port")
+
+    if scan_type == "simple":
+        scan_port(target_ip, int(target_port))
